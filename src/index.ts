6dd1dae63672dbf568cc3082a879d2c5a7fd97f3
Migue <migue@pop-os.localdomain>
2022-12-12 07:46:55 -0300

    add socket

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,33 @@
 import express, { Express, Request, Response } from 'express';
+import http from "http";
+import { Server, Socket } from "socket.io";
 
 const app: Express = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
+
+io.on("connection", (socket: Socket) => {
+  console.log("Socket connected: " + socket.id);
+  socket.on("comment:show", (comment) => {
+    io.emit("comment:show", comment);
+  })
+  socket.on("comment:clear", () => {
+    io.emit("comment:clear");
+  })
+})
+
+
+server.listen(port, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
